20bfe60068490ef82b68db378f03c0f9a92c8e14
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2023-10-23 20:59:09 +0700

    Initial Commit

---
+++ Wordy Word (SwiftUI)/ContentView.swift
@@ -0,0 +1,29 @@
+//  
+//  ContentView.swift
+//  Wordy Word (SwiftUI)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 23/10/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wordy Word (SwiftUI)/Wordy_Word__SwiftUI_App.swift
@@ -0,0 +1,22 @@
+//  
+//  Wordy_Word__SwiftUI_App.swift
+//  Wordy Word (SwiftUI)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 23/10/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+@main
+struct Wordy_Word__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
