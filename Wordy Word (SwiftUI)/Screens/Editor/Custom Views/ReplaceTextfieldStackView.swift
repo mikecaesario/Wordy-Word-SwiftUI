e150305bf9dde6a0e8d505c53cbc263d4e7a3533
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2023-11-04 02:00:22 +0700

    added replacetextfieldstack

---
+++ Wordy Word (SwiftUI)/Screens/Editor/Custom Views/ReplaceTextfieldStackView.swift
@@ -0,0 +1,23 @@
+//  
+//  ReplaceTextfieldStackView.swift
+//  Wordy Word (SwiftUI)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 03/11/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+struct ReplaceTextfieldStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReplaceTextfieldStackView()
+}
