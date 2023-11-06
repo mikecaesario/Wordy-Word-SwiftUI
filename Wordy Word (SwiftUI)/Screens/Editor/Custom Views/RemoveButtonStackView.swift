dc809eb731f23f1babd4ee8462abfefcb7caf4d0
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2023-11-06 23:19:53 +0700

    ui fixes

---
+++ Wordy Word (SwiftUI)/Screens/Editor/Custom Views/RemoveButtonStackView.swift
@@ -0,0 +1,23 @@
+//  
+//  RemoveButtonStackView.swift
+//  Wordy Word (SwiftUI)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 04/11/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import SwiftUI
+
+struct RemoveButtonStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RemoveButtonStackView()
+}
