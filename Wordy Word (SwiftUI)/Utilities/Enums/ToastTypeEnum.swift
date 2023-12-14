8b06d8430ad58fbe86195092ade41e5452a139dd
Michael Caesario <53443317+mikecaesario@users.noreply.github.com>
2023-12-14 22:45:32 +0700

    final commit

---
+++ Wordy Word (SwiftUI)/Utilities/Enums/ToastTypeEnum.swift
@@ -0,0 +1,13 @@
+//  
+//  ToastTypeEnum.swift
+//  Wordy Word (SwiftUI)
+//
+//  Created with ❤️‍🔥 by Michael Caesario on 14/12/23.
+//  Copyright © 2023 Michael Caesario. All rights reserved.
+// 
+//  Website: https://mikecaesario.app
+//  GitHub: https://github.com/mikecaesario
+//  LinkedIn: https://www.linkedin.com/in/mikecaesario/
+//
+
+import Foundation
